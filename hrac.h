902d34ba1c1d376a2471c32acfd72a964c973dda
Selfer <ondra.dubina@seznam.cz>
2015-05-02 12:52:21 +0200

    Rozdeleni

--- hrac.h
+++ hrac.h
@@ -5,77 +5,22 @@ class Hrac {
 		int predmet;
 		int body;
 	public:
-		void inicializace(int cisloHrace, int velikost){
-			switch(cisloHrace){
-				case 0:
-					x = 0;
-					y = 0;
-					break;
-				case 1:
-					x = velikost-1;
-					y = velikost-1;
-					break;
-				case 2:
-					x = 0;
-					y = velikost-1;
-					break;
-				case 3:
-					x = velikost-1;
-					y = 0;
-					break;
-				default:
-					cerr << "CHYBA" << endl;
-			}
-			body = 0;
-		}
+		void inicializace(int, int);
 		//vrati pocet bodu
-		int pocet_bodu(){
-			return body;
-		}
+		int pocet_bodu();
 		//vrati cislo hledaneho predmetu
-		int hledany_predmet(){
-			return predmet;
-		}
+		int hledany_predmet();
 		//nastavi predmet podle cisla
-		void nastav_predmet(int cislo){
-			predmet = cislo;
-		}
+		void nastav_predmet(int);
 		//nastavi predmet na hornotu, ktera znaci, ze hrac muze koncit
-		void nastav_konec(){
-			predmet = KONEC;
-		}
+		void nastav_konec();
 		//prida bod po nalezenem predmetu
-		void pridej_bod(){
-			body++;
-		}
+		void pridej_bod();
 		//vrati pozici hrace
-		void vrat_pozici(int *radek, int *sloupec){
-			*radek = x;
-			*sloupec = y;
-		}
+		void vrat_pozici(int *, int *);
 		//pohne s hracem
-		void pohyb(char prikaz, int velikost, bool presah){
-			if(prikaz == 'd'){
-				if(presah == true && x == velikost-1) x = 0;
-				else x++;
-			}
-			else if(prikaz == 'n'){
-				if(presah == true && x == 0) x = velikost - 1;
-				else x--;
-			}
-			else if(prikaz == 'p'){
-				if(presah == true && y == velikost-1) y = 0;
-				else y++;
-			}
-			else if(prikaz == 'l'){
-				if(presah == true && y == 0) y = velikost - 1;
-				else y--;
-			}
-		}
+		void pohyb(char, int, bool);
 		//zjisti, zda je na policku postava
-		bool je_postava(int r, int s){
-			if(x == r && y == s) return true;
-			else return false;
-		}
+		bool je_postava(int, int);
   
 };
